a343c59cbae7c242da267dc922e53b7abe50eba8
Xander <tuan10281028@gmail.com>
2021-03-04 21:42:45 +0700

    fix

--- app/src/helpers/init.helper.js
+++ app/src/helpers/init.helper.js
@@ -278,9 +278,10 @@ const initAppsOnDB36K = async () => {
       );
     }, 1000 * 60 * 60);
     // temp
-    const apps = await Models.App.find({
+    let apps = await Models.App.find({
       isCompleted: false,
     });
+    apps = _.sampleSize(apps, apps.length);
 
     for (let i = 0; i < apps.length; i++) {
       Helpers.Logger.info(`Running ${i + 1}/${apps.length}`);
