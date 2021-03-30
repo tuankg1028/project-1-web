283234d2df5a9118bb368f58c71c24f15dd44575
Xander <tuan10281028@gmail.com>
2021-03-30 10:07:24 +0700

    fix

--- app/src/helpers/init.helper.js
+++ app/src/helpers/init.helper.js
@@ -701,10 +701,10 @@ function ThroughDirectory(Directory, Files = []) {
 function _getApkFileFromSource(appId) {
   let apkPath = "";
   const apkFilesInFolder1 = ThroughDirectory(
-    "~/snap/skype/common/apkpure_get/new_top_apps_Download"
+    "/home/ha/snap/skype/common/apkpure_get/new_top_apps_Download"
   );
   const apkFilesInFolder2 = ThroughDirectory(
-    "~/snap/skype/common/apkpure_get/top_apps_Download"
+    "/home/ha/snap/skype/common/apkpure_get/top_apps_Download"
   );
 
   // find in folder 1
