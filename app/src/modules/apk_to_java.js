12074aa27cfd4587ecc0f92b0ccef28713f03d51
Xander <xander@vinova.com.sg>
2021-04-20 22:59:17 +0700

    apk to java

---
+++ app/src/modules/apk_to_java.js
@@ -0,0 +1,43 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const apkFolders = [
+  "/home/ha/tuan/projects/project-1-web/malware/kuafuDet/benign500",
+  "/home/ha/tuan/projects/project-1-web/malware/kuafuDet/StormDroid&KuafuDet_2082",
+];
+async function main() {
+  // execSync(
+  //   `sh ./jadx/build/jadx/bin/jadx -d "${apkSourcePath}" "${pathFileApk}"`
+  // );
+  for (let i = 0; i < apkFolders.length; i++) {
+    const apkFolder = apkFolders[i];
+
+    const outputFolderPath = path.join(
+      path.dirname(apkFolder),
+      "/JavaSources/",
+      path.basename(apkFolder)
+    );
+    try {
+      execSync(`mkdir ${path.dirname(outputFolderPath)}`);
+    } catch (e) {
+      console.log(e.message);
+    }
+    try {
+      execSync(`mkdir ${outputFolderPath}`);
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    let apkFiles = fs.readdirSync(apkFolder);
+    apkFiles = apkFiles.filter((filename) => filename.endsWith("apk"));
+
+    apkFiles.forEach((apkFile) => {
+      const outputPath = path.join(outputFolderPath, path.basename(apkFile));
+
+      const jadxFolder = path.join(__dirname, "../../jadx/build/jadx/bin/jadx");
+      execSync(`sh ${jadxFolder} -d "${outputPath}" "${apkFile}"`);
+    });
+  }
+}
+
+main();
