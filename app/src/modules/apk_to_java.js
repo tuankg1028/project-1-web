be27cf64d9b01d5469943803be063d82bc97d865
Xander <xander@vinova.com.sg>
2021-04-21 10:44:03 +0700

    fix

--- app/src/modules/apk_to_java.js
+++ app/src/modules/apk_to_java.js
@@ -4,7 +4,13 @@ const path = require("path");
 const apkFolders = [
   "/home/ha/tuan/projects/project-1-web/malware/kuafuDet/benign500",
   "/home/ha/tuan/projects/project-1-web/malware/kuafuDet/StormDroid&KuafuDet_2082",
+  // "/Users/a1234/individual/abc/project-1-web/app/sourceTemp",
 ];
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const permissions = require("../../data/apktojava/System permissions.json");
+const Helpers = require("../helpers");
+const slug = require("slug");
+
 async function main() {
   // execSync(
   //   `sh ./jadx/build/jadx/bin/jadx -d "${apkSourcePath}" "${pathFileApk}"`
@@ -41,6 +47,8 @@ async function main() {
         );
         const apkFileFullPath = `${apkFolder}/${apkFile}`;
         execSync(`sh ${jadxFolder} -d "${outputPath}" "${apkFileFullPath}"`);
+        // execSync(`jadx -d "${outputPath}" "${apkFileFullPath}"`);
+
         console.log(
           `DONE ${path.basename(apkFile)} in ${path.basename(apkFolder)} folder`
         );
@@ -50,5 +58,66 @@ async function main() {
     });
   }
 }
+// main();
 
-main();
+async function main2() {
+  const header = [
+    {
+      id: "stt",
+      title: "#",
+    },
+    {
+      id: "appName",
+      title: "App name",
+    },
+    ...(permissions &&
+      permissions.map((item) => ({ id: slug(item), title: item }))),
+  ];
+  const rows = [];
+
+  let stt = 1;
+  apkFolders.forEach((apkFolder) => {
+    const outputFolderPath = path.join(
+      path.dirname(apkFolder),
+      "/JavaSources/",
+      path.basename(apkFolder)
+    );
+    let apkFiles = fs.readdirSync(apkFolder);
+    apkFiles = apkFiles.filter((filename) => filename.endsWith("apk"));
+    apkFiles.forEach((apkFile) => {
+      try {
+        let row = {
+          stt,
+          appName: path.basename(apkFile),
+        };
+        stt++;
+        const outputPath = path.join(outputFolderPath, path.basename(apkFile));
+        let content = Helpers.default.File.getContentOfFolder(
+          `${outputPath}/sources`
+        );
+        content = content.toLowerCase();
+        // check perrmission exists in content
+        permissions.forEach((permission) => {
+          if (~content.indexOf(permission.toLowerCase())) {
+            row[slug(permission)] = 1;
+          } else {
+            row[slug(permission)] = 0;
+          }
+        });
+
+        rows.push(row);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
+
+  const csvWriter = createCsvWriter({
+    path: "apps-permissions.csv",
+    header,
+  });
+  await csvWriter.writeRecords(rows);
+  console.log("==== DONE ====");
+}
+
+main2();
