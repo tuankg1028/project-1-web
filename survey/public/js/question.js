00a0b5f05a5cfdd6489a2ac7ea9f5fdc05b334a9
Xander <xander@vinova.com.sg>
2021-05-24 14:41:06 +0700

    fix

--- survey/public/js/question.js
+++ survey/public/js/question.js
@@ -229,8 +229,8 @@
             <!-- anwsers-->
             <div class="anwsers mt-2">
                 ${ourPrediction == 1 ? "" : `<label class="container-radio">Yes<input class="final-question" type="radio" name="questions[${appId}][install]" value="1" required="required" ${installtionAnswer == 1 ? "checked": ""} ><span class="checkmark"></span></label>`}
-                ${ourPrediction == 0 ? "" : `<label class="container-radio">No<input class="final-question" type="radio" name="questions[${appId}][install]" value="0"  ${installtionAnswer == 0 && installtionAnswer !== "" ? "checked" : ""} ><span class="checkmark"></span></label>`}
-                ${ourPrediction == 2 ? "" : `<label class="container-radio">Maybe<input class="final-question" type="radio" name="questions[${appId}][install]" value="2"  ${installtionAnswer == 2 ? "checked": ""} ><span class="checkmark"></span></label>`}
+                ${ourPrediction == 0 ? "" : `<label class="container-radio">No<input class="final-question" type="radio" name="questions[${appId}][install]" value="0" required="required" ${installtionAnswer == 0 && installtionAnswer !== "" ? "checked" : ""} ><span class="checkmark"></span></label>`}
+                ${ourPrediction == 2 ? "" : `<label class="container-radio">Maybe<input class="final-question" type="radio" name="questions[${appId}][install]" value="2" required="required" ${installtionAnswer == 2 ? "checked" : ""} ><span class="checkmark"></span></label>`}
             </div>
           </div>
           `
