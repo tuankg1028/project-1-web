6bce1143833c047ffe090304f0701c29eb84b0d5
Xander <xander@vinova.com.sg>
2021-07-12 13:02:38 +0700

    fix

--- survey/src/stats/index.js
+++ survey/src/stats/index.js
@@ -2108,7 +2108,7 @@ async function calculateAccuranceByTranningApps() {
           60 * 60 * 24 * 30
         ); // 1 month;
 
-        tranningAppIds = user.questionIds.slice(0, 10);
+        let tranningAppIds = user.questionIds.slice(0, 10);
 
         let ourPrediction;
         // try {
