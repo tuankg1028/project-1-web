1b7019377ec0ac5cfc7e26bc73ecd2a00c24bc9a
Xander <xander@vinova.com.sg>
2021-11-10 20:07:44 +0700

    fix

--- app/src/index.js
+++ app/src/index.js
@@ -93,7 +93,7 @@ async function main() {
     appsApis = [...appsApis, ...appApis];
 
     skip += 100;
-    apps = null;
+    apps = [];
     // apps = await Models.App.find({}).limit(100).skip(skip);
   }
 
