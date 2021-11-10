77262263ee80fa6d729ed9943647fe7f9ead44ca
Xander <xander@vinova.com.sg>
2021-11-10 21:44:46 +0700

    fix

--- app/src/index.js
+++ app/src/index.js
@@ -76,7 +76,7 @@ async function main() {
   }).cache(60 * 60 * 24 * 30);
 
   let skip = 0;
-  let apps = await Models.App.find({}).limit(3000).skip(skip);
+  let apps = await Models.App.find({}).limit(1000).skip(skip);
   let appsApis = [];
   while (apps.length) {
     const appApis = apps.map((app) => {
@@ -96,12 +96,12 @@ async function main() {
     appsApis = [...appsApis, ...appApis];
 
     skip += 1000;
-    apps = [];
-    // apps = await Models.App.find({}).limit(1000).skip(skip);
+    // apps = [];
+    apps = await Models.App.find({}).limit(1000).skip(skip);
   }
 
   console.log("RUNNING data TYpe");
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < tree.length; i++) {
     const dataType = tree[i];
     console.log(dataType.name);
     const cloneAppsApis = JSON.parse(JSON.stringify(appsApis));
