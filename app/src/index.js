aff056f171e8fb2a46b19b8b88c0ed9f98f74c07
Xander <xander@vinova.com.sg>
2021-11-11 21:15:40 +0700

    fix

--- app/src/index.js
+++ app/src/index.js
@@ -142,6 +142,13 @@ async function main() {
         if (apisApp.apis.includes(api.name.trim().replace(".", ""))) {
           result[dataType.name].apis[indexApi].count++;
         }
+
+        if (api.name === "com.google.android.gms.fitness.data") {
+          console.log(
+            apisApp.apis.includes(api.name.trim().replace(".", "")),
+            apisApp.apis;
+          );
+        }
       }
 
       // classes
