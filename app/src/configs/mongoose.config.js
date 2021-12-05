a57bfc15e7b2bc382f97110e77b8306f12456020
Xander <xander@vinova.com.sg>
2021-12-05 23:12:44 +0700

    fix

--- app/src/configs/mongoose.config.js
+++ app/src/configs/mongoose.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 import mongoose from "mongoose";
 import Helpers from "../helpers";
-import MongooseCache from "mongoose-redis";
+// import MongooseCache from "mongoose-redis";
 
 const { MONGODB_URL, REDIS_HOST, REDIS_PORT } = process.env;
 
@@ -16,4 +16,4 @@ mongoose.connection.on("error", (err) => {
 });
 mongoose.set("debug", true);
 
-MongooseCache(mongoose, `redis://${REDIS_HOST}:${REDIS_PORT}`);
+// MongooseCache(mongoose, `redis://${REDIS_HOST}:${REDIS_PORT}`);
