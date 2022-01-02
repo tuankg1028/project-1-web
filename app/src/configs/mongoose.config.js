99e92db552edfa50c0fd5def7fe519a6b2ffaa9f
Xander <xander@vinova.com.sg>
2022-01-02 22:29:08 +0700

    fix

--- app/src/configs/mongoose.config.js
+++ app/src/configs/mongoose.config.js
@@ -14,6 +14,6 @@ mongoose.connect(MONGODB_URL, {
 mongoose.connection.on("error", (err) => {
   Helpers.Logger.error(err);
 });
-mongoose.set("debug", false);
+mongoose.set("debug", true);
 
 // MongooseCache(mongoose, `redis://${REDIS_HOST}:${REDIS_PORT}`);
