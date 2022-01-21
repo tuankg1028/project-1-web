2fcbc534342d96b4a1dbe37ff8981d5ccc44e87d
Xander <xander@vinova.com.sg>
2022-01-21 13:19:39 +0700

    update

--- app/src/modules/getAppComment.js
+++ app/src/modules/getAppComment.js
@@ -256,33 +256,9 @@ async function statCommentsUserByKeywordsV2() {
     ])
     
     const appCommentsGroup = _.groupBy(comments, 'appId')
-    for (const appId in appCommentsGroup) {
-        const appComments = appCommentsGroup[appId];
 
-        let [app, totalComments] = await Promise.all([
-            Models.App.findById(appId).select('appName'),
-            Models.AppComment.find({
-                appId
-            }).select('text')
-        ])
-        
-        totalComments = totalComments.filter(item => {
-            if(!item.text) return false
-            let text = item.text.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/:-?[()pPdD]/gi, '');
-           
-            if(text.split(' ').length <= 3) return true
-
-            text = removeEmojis(text)
-            console.log(isEnglish(text), text)
-            return isEnglish(text)
-        })
-
-        rows.push({
-            appName: app.appName,
-            totalComment: totalComments.length,
-            totalCommentKeywords: appComments.length,
-        })
-    }
+    await Promise.all(Object.entries(appCommentsGroup).map(buildRow))
+    
     
     rows = _.orderBy(rows, 'totalCommentKeywords', 'desc')
     rows = rows.map((row, i) => ({...row, stt: i + 1}))
@@ -294,7 +270,31 @@ async function statCommentsUserByKeywordsV2() {
 
     console.log("DONE statCommentsUserByKeywords")
 }
+async function buildRow(appId, appComments) {
+    let [app, totalComments] = await Promise.all([
+        Models.App.findById(appId).select('appName'),
+        Models.AppComment.find({
+            appId
+        }).select('text')
+    ])
+    
+    totalComments = totalComments.filter(item => {
+        if(!item.text) return false
+        let text = item.text.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/:-?[()pPdD]/gi, '');
+        
+        if(text.split(' ').length <= 3) return true
 
+        text = removeEmojis(text)
+        console.log(isEnglish(text), text)
+        return isEnglish(text)
+    })
+
+    return {
+        appName: app.appName,
+        totalComment: totalComments.length,
+        totalCommentKeywords: appComments.length,
+    }
+}
 function removeEmojis(string) {
     var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
     return string.replace(regex, "");
