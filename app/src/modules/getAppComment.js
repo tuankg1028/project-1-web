7f0cbe6b30d541e022c5a62d4f1801d86492849e
Xander <xander@vinova.com.sg>
2022-02-08 20:27:14 +0700

    fix

--- app/src/modules/getAppComment.js
+++ app/src/modules/getAppComment.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
-// import "../configs/mongoose.config";
+import "../configs/mongoose.config";
 import axios from "axios";
 const fs = require("fs");
 const path = require("path");
 var parseString = require("xml2js").parseString;
 const _ = require("lodash");
-// import Models from "../models";
+import Models from "../models";
 import Helpers from "../helpers";
 import natural from "natural";
-var bayesClassifier = new natural.BayesClassifier();
-var logisticRegressionClassifier = new natural.LogisticRegressionClassifier();
+const PorterStemmer = require('../../node_modules/natural/lib/natural/stemmers/porter_stemmer');
+
+var bayesClassifier = new natural.BayesClassifier(PorterStemmer);
+var logisticRegressionClassifier = new natural.LogisticRegressionClassifier(PorterStemmer);
 
 import readXlsxFile from "read-excel-file/node";
 
@@ -32,12 +34,13 @@ async function main() {
   // statCommentsByUsers()
   // statCommentsUserByKeywords();
   // statCommentsUserByKeywordsV2()
-  // statCommentsUserByKeywordsLabel();
-  trainningAndTesting();
+  statCommentsUserByKeywordsLabel();
+  // trainningAndTesting();
 }
 
 async function trainningAndTesting() {
   console.log("Running trainningAndTesting");
+  const percentage = 60
   const result = {
     bayes: {
       TP: 0,
@@ -67,17 +70,36 @@ async function trainningAndTesting() {
     },
   ];
 
-  const numberTrainning = 594;
-
   const fileData = await csv({
     noheader: true,
     output: "csv",
-  }).fromFile("/Users/a1234/Downloads/TRAINING-TEST.csv");
+  }).fromFile("/Users/xander/Downloads/TRAINING-TEST.csv");
 
   let [, ...rows] = fileData;
 
-  const trainning = rows.splice(0, numberTrainning);
+  const totalY = rows.filter(row => row[6] === 'Y').length
+  const totalN = rows.filter(row => row[6] === 'N').length
+  
+  let rowsCSV = []
+  
+  rows = _.orderBy(rows, (row => row[6]), 'desc')
+  const trainningY = rows.splice(0, Math.floor(totalY* (percentage / 100)));
+  
+  rows = _.orderBy(rows, (row => row[6]), 'asc')
+  const trainningN = rows.splice(0, Math.floor(totalN* (percentage / 100)));
+  
 
+  const trainning = [...trainningY, ...trainningN]
+  trainning.forEach((item, index) => {
+    rowsCSV.push({
+        userName: item[1],
+        comment: item[2],
+        appName: item[3],
+        rating: item[4],
+        thumbsUp: item[5],
+        label: item[6],
+    })
+  })
   trainning.forEach(([, , text, , , , label]) => {
     bayesClassifier.addDocument(text, label);
     logisticRegressionClassifier.addDocument(text, label);
@@ -86,7 +108,8 @@ async function trainningAndTesting() {
   logisticRegressionClassifier.train();
 
   const testing = rows;
-  testing.forEach(([, , text, , , , label]) => {
+  testing.forEach((item) => {
+      const [, , text, , , , label] = item
     const actualClassBayes = bayesClassifier.classify(text);
 
     if (label === "Y" && actualClassBayes == "Y") result.bayes.TP++;
@@ -99,6 +122,17 @@ async function trainningAndTesting() {
     else if (label === "N" && actualClassLogistic == "N") result.logistic.TN++;
     else if (label === "Y" && actualClassLogistic == "N") result.logistic.FP++;
     else if (label === "N" && actualClassLogistic == "Y") result.logistic.FN++;
+
+    rowsCSV.push({
+        userName: item[1],
+        comment: item[2],
+        appName: item[3],
+        rating: item[4],
+        thumbsUp: item[5],
+        label: item[6],
+        bayesian: actualClassBayes,
+        logistic: actualClassLogistic
+    })
   });
 
   // accuracy
@@ -170,13 +204,62 @@ async function trainningAndTesting() {
   ];
 
   const csvWriterAccuracy = createCsvWriter({
-    path: "./output/Bayesian-and-Logistic-Regression-Classifiers.csv",
+    path: `./output/Bayesian-and-Logistic-Regression-Classifiers(${percentage}-${100 - percentage}).csv`,
     header: headerAccuracy,
   });
   await csvWriterAccuracy.writeRecords(rowsAccuracy);
 
+  rowsCSV = rowsCSV.map((item, index) => {
+    item.stt = index + 1
+    return item
+  })
+  const csvWriter = createCsvWriter({
+    path: `./output/TRAINING-TEST(${percentage}-${100 -percentage}).csv`,
+    header: [
+        {
+          id: "stt",
+          title: "#",
+        },
+        {
+          id: "userName",
+          title: "User Name",
+        },
+        {
+          id: "comment",
+          title: "Comment",
+        },
+        {
+          id: "appName",
+          title: "App Name",
+        },
+        {
+          id: "rating",
+          title: "Rating",
+        },
+        {
+          id: "thumbsUp",
+          title: "Thumbs Up",
+        },
+        {
+            id: "label",
+            title: "Label (Y/N)",
+          },
+        {
+            id: "bayesian",
+            title: "Bayesian",
+        },
+        {
+            id: "logistic",
+            title: "Logistic Regression",
+        },
+      ],
+  });
+  await csvWriter.writeRecords(rowsCSV);
+
   console.log("DONE");
 }
+
+
 async function statCommentsUserByKeywordsLabel() {
   let apps = await Models.App.find({
     isExistedMobiPurpose: true,
@@ -635,181 +718,7 @@ function getCommonWords(comments) {
   }, []);
 }
 function removeStopWords(comments) {
-  const words = [
-    "a",
-    "about",
-    "above",
-    "after",
-    "again",
-    "against",
-    "all",
-    "am",
-    "an",
-    "and",
-    "any",
-    "are",
-    "aren't",
-    "as",
-    "at",
-    "be",
-    "because",
-    "been",
-    "before",
-    "being",
-    "below",
-    "between",
-    "both",
-    "but",
-    "by",
-    "can't",
-    "cannot",
-    "could",
-    "couldn't",
-    "did",
-    "didn't",
-    "do",
-    "does",
-    "doesn't",
-    "doing",
-    "don't",
-    "down",
-    "during",
-    "each",
-    "few",
-    "for",
-    "from",
-    "further",
-    "had",
-    "hadn't",
-    "has",
-    "hasn't",
-    "have",
-    "haven't",
-    "having",
-    "he",
-    "he'd",
-    "he'll",
-    "he's",
-    "her",
-    "here",
-    "here's",
-    "hers",
-    "herself",
-    "him",
-    "himself",
-    "his",
-    "how",
-    "how's",
-    "i",
-    "i'd",
-    "i'll",
-    "i'm",
-    "i've",
-    "if",
-    "in",
-    "into",
-    "is",
-    "isn't",
-    "it",
-    "it's",
-    "its",
-    "itself",
-    "let's",
-    "me",
-    "more",
-    "most",
-    "mustn't",
-    "my",
-    "myself",
-    "no",
-    "nor",
-    "not",
-    "of",
-    "off",
-    "on",
-    "once",
-    "only",
-    "or",
-    "other",
-    "ought",
-    "our",
-    "ours	ourselves",
-    "out",
-    "over",
-    "own",
-    "same",
-    "shan't",
-    "she",
-    "she'd",
-    "she'll",
-    "she's",
-    "should",
-    "shouldn't",
-    "so",
-    "some",
-    "such",
-    "than",
-    "that",
-    "that's",
-    "the",
-    "their",
-    "theirs",
-    "them",
-    "themselves",
-    "then",
-    "there",
-    "there's",
-    "these",
-    "they",
-    "they'd",
-    "they'll",
-    "they're",
-    "they've",
-    "this",
-    "those",
-    "through",
-    "to",
-    "too",
-    "under",
-    "until",
-    "up",
-    "very",
-    "was",
-    "wasn't",
-    "we",
-    "we'd",
-    "we'll",
-    "we're",
-    "we've",
-    "were",
-    "weren't",
-    "what",
-    "what's",
-    "when",
-    "when's",
-    "where",
-    "where's",
-    "which",
-    "while",
-    "who",
-    "who's",
-    "whom",
-    "why",
-    "why's",
-    "with",
-    "won't",
-    "would",
-    "wouldn't",
-    "you",
-    "you'd",
-    "you'll",
-    "you're",
-    "you've",
-    "your",
-    "yours",
-    "yourself",
-    "yourselves",
-  ];
+  const words = ["able","about","above","abroad","according","accordingly","across","actually","adj","after","afterwards","again","against","ago","ahead","ain't","all","allow","allows","almost","alone","along","alongside","already","also","although","always","am","amid","amidst","among","amongst","an","and","another","any","anybody","anyhow","anyone","anything","anyway","anyways","anywhere","apart","appear","appreciate","appropriate","are","aren't","around","as","a's","aside","ask","asking","associated","at","available","away","awfully","back","backward","backwards","be","became","because","become","becomes","becoming","been","before","beforehand","begin","behind","being","believe","below","beside","besides","best","better","between","beyond","both","brief","but","by","came","can","cannot","cant","can't","caption","cause","causes","certain","certainly","changes","clearly","c'mon","co","co.","com","come","comes","concerning","consequently","consider","considering","contain","containing","contains","corresponding","could","couldn't","course","c's","currently","dare","daren't","definitely","described","despite","did","didn't","different","directly","do","does","doesn't","doing","done","don't","down","downwards","during","each","edu","eg","eight","eighty","either","else","elsewhere","end","ending","enough","entirely","especially","et","etc","even","ever","evermore","every","everybody","everyone","everything","everywhere","ex","exactly","example","except","fairly","far","farther","few","fewer","fifth","first","five","followed","following","follows","for","forever","former","formerly","forth","forward","found","four","from","further","furthermore","get","gets","getting","given","gives","go","goes","going","gone","got","gotten","greetings","had","hadn't","half","happens","hardly","has","hasn't","have","haven't","having","he","he'd","he'll","hello","help","hence","her","here","hereafter","hereby","herein","here's","hereupon","hers","herself","he's","hi","him","himself","his","hither","hopefully","how","howbeit","however","hundred","i'd","ie","if","ignored","i'll","i'm","immediate","in","inasmuch","inc","inc.","indeed","indicate","indicated","indicates","inner","inside","insofar","instead","into","inward","is","isn't","it","it'd","it'll","its","it's","itself","i've","just","k","keep","keeps","kept","know","known","knows","last","lately","later","latter","latterly","least","less","lest","let","let's","like","liked","likely","likewise","little","look","looking","looks","low","lower","ltd","made","mainly","make","makes","many","may","maybe","mayn't","me","mean","meantime","meanwhile","merely","might","mightn't","mine","minus","miss","more","moreover","most","mostly","mr","mrs","much","must","mustn't","my","myself","name","namely","nd","near","nearly","necessary","need","needn't","needs","neither","never","neverf","neverless","nevertheless","new","next","nine","ninety","no","nobody","non","none","nonetheless","noone","no-one","nor","normally","not","nothing","notwithstanding","novel","now","nowhere","obviously","of","off","often","oh","ok","okay","old","on","once","one","ones","one's","only","onto","opposite","or","other","others","otherwise","ought","oughtn't","our","ours","ourselves","out","outside","over","overall","own","particular","particularly","past","per","perhaps","placed","please","plus","possible","presumably","probably","provided","provides","que","quite","qv","rather","rd","re","really","reasonably","recent","recently","regarding","regardless","regards","relatively","respectively","right","round","said","same","saw","say","saying","says","second","secondly","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sensible","sent","serious","seriously","seven","several","shall","shan't","she","she'd","she'll","she's","should","shouldn't","since","six","so","some","somebody","someday","somehow","someone","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specified","specify","specifying","still","sub","such","sup","sure","take","taken","taking","tell","tends","th","than","thank","thanks","thanx","that","that'll","thats","that's","that've","the","their","theirs","them","themselves","then","thence","there","thereafter","thereby","there'd","therefore","therein","there'll","there're","theres","there's","thereupon","there've","these","they","they'd","they'll","they're","they've","thing","things","think","third","thirty","this","thorough","thoroughly","those","though","three","through","throughout","thru","thus","till","to","together","too","took","toward","towards","tried","tries","truly","try","trying","t's","twice","two","un","under","underneath","undoing","unfortunately","unless","unlike","unlikely","until","unto","up","upon","upwards","us","use","used","useful","uses","using","usually","v","value","various","versus","very","via","viz","vs","want","wants","was","wasn't","way","we","we'd","welcome","well","we'll","went","were","we're","weren't","we've","what","whatever","what'll","what's","what've","when","whence","whenever","where","whereafter","whereas","whereby","wherein","where's","whereupon","wherever","whether","which","whichever","while","whilst","whither","who","who'd","whoever","whole","who'll","whom","whomever","who's","whose","why","will","willing","wish","with","within","without","wonder","won't","would","wouldn't","yes","yet","you","you'd","you'll","your","you're","yours","yourself","yourselves","you've","zero","a","how's","i","when's","why's","b","c","d","e","f","g","h","j","l","m","n","o","p","q","r","s","t","u","uucp","w","x","y","z","I","www","amount","bill","bottom","call","computer","con","couldnt","cry","de","describe","detail","due","eleven","empty","fifteen","fifty","fill","find","fire","forty","front","full","give","hasnt","herse","himse","interest","itse”","mill","move","myse”","part","put","show","side","sincere","sixty","system","ten","thick","thin","top","twelve","twenty","abst","accordance","act","added","adopted","affected","affecting","affects","ah","announce","anymore","apparently","approximately","aren","arent","arise","auth","beginning","beginnings","begins","biol","briefly","ca","date","ed","effect","et-al","ff","fix","gave","giving","heres","hes","hid","home","id","im","immediately","importance","important","index","information","invention","itd","keys","kg","km","largely","lets","line","'ll","means","mg","million","ml","mug","na","nay","necessarily","nos","noted","obtain","obtained","omitted","ord","owing","page","pages","poorly","possibly","potentially","pp","predominantly","present","previously","primarily","promptly","proud","quickly","ran","readily","ref","refs","related","research","resulted","resulting","results","run","sec","section","shed","shes","showed","shown","showns","shows","significant","significantly","similar","similarly","slightly","somethan","specifically","state","states","stop","strongly","substantially","successfully","sufficiently","suggest","thered","thereof","therere","thereto","theyd","theyre","thou","thoughh","thousand","throug","til","tip","ts","ups","usefully","usefulness","'ve","vol","vols","wed","whats","wheres","whim","whod","whos","widely","words","world","youd","youre"]
 
   comments = comments.map((item) => {
     words.forEach((word) => {
