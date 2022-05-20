51294158d51b884b523c73855173ecb1905348f4
nb91265 <nb91265@navercorp.com>
2022-05-20 20:16:57 +0700

    fix

---
+++ app/src/modules/count-function.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+import "../configs/mongoose.config";
+import Models from "../models";
+import _ from "lodash";
+import Helpers from "../helpers";
+
+main();
+async function main() {
+  let apps = await Models.App.find({
+    categoryName: "Beauty",
+  }).limit(10);
+
+  for (let i = 0; i < apps.length; i++) {
+    try {
+      const app = apps[i];
+
+      const apkSourcePath = `/data/JavaCode/${app.id}`;
+      console.log(apkSourcePath);
+      const contents = await Helpers.File.getContentOfFolder(
+        `${apkSourcePath}/sources`
+      );
+
+      if (contents) console.log("YES");
+    } catch (err) {
+      console.log("NO");
+    }
+  }
+}
