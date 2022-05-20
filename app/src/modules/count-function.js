d659eeb27979ab084c2c28455bf62e636ac11fe1
nb91265 <nb91265@navercorp.com>
2022-05-20 20:28:00 +0700

    add key pair

--- app/src/modules/count-function.js
+++ app/src/modules/count-function.js
@@ -3,6 +3,7 @@ import "../configs/mongoose.config";
 import Models from "../models";
 import _ from "lodash";
 import Helpers from "../helpers";
+const fs = require("fs");
 
 main();
 async function main() {
@@ -14,7 +15,19 @@ async function main() {
     try {
       const app = apps[i];
 
+      const pathFileApk = _getApkFileFromSource(app.id, app.appName);
+      console.log(pathFileApk);
+      if (!pathFileApk) throw new Error("Cannot find apk file");
+      Helpers.Logger.step("Step 2: Parse APK to Text files by jadx");
+
       const apkSourcePath = `/data/JavaCode/${app.id}`;
+      if (!fs.existsSync(apkSourcePath)) execSync(`mkdir ${apkSourcePath}`);
+      const jadxScript = `sh ./jadx/build/jadx/bin/jadx -d "${apkSourcePath}" "${pathFileApk}"`;
+      console.log("jadxScript", jadxScript);
+      execSync(jadxScript, {
+        timeout: 1000 * 60 * 5, // 5 mins
+      });
+
       console.log(apkSourcePath);
       const contents = await Helpers.File.getContentOfFolder(
         `${apkSourcePath}/sources`
@@ -22,7 +35,49 @@ async function main() {
 
       if (contents) console.log("YES");
     } catch (err) {
+			console.log(err);
       console.log("NO");
     }
   }
+}
+
+function ThroughDirectory(Directory, Files = []) {
+  fs.readdirSync(Directory).forEach((File) => {
+    const Absolute = path.join(Directory, File);
+    if (fs.statSync(Absolute).isDirectory())
+      return ThroughDirectory(Absolute, Files);
+    else Files.push(Absolute);
+  });
+
+  return Files;
+}
+
+function _getApkFileFromSource(appId, appName) {
+  let apkPath = "";
+  const apkFilesInFolder1 = ThroughDirectory(
+    "/data/apkfile/new_top_apps_Download"
+  );
+  const apkFilesInFolder2 = ThroughDirectory("/data/apkfile/top_apps_Download");
+  const apkFilesInFolder3 = ThroughDirectory("/data/apkfile/mobipurpose-apks");
+  const apkFilesInFolder4 = ThroughDirectory(
+    "/home/son/apkfile/mobipurpose-apks"
+  );
+
+  const apkFiles = [
+    ...apkFilesInFolder1,
+    ...apkFilesInFolder2,
+    ...apkFilesInFolder3,
+    ...apkFilesInFolder4,
+  ];
+
+  // find in folder
+  for (let i = 0; i < apkFiles.length; i++) {
+    const apkFile = apkFiles[i];
+
+    if (apkFile.includes(appId) || apkFile.includes(appName)) {
+      apkPath = apkFile;
+      break;
+    }
+  }
+  return apkPath;
 }
